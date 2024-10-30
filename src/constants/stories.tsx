c319185ed25d6b5bad751cd2dd5eeb95b1699035
devAlen01 <alenermekuulu@gmail.com>
2024-10-30 12:59:39 +0600

    ....

--- src/constants/stories.tsx
+++ src/constants/stories.tsx
@@ -42,4 +42,41 @@ export const stories = [
       "https://ew.com/thmb/O5LusHiubmFfxX6DBjl4t56Bbgw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Chris-Evans-060124-800c2c0237574b2c81c140393ba20555.jpg",
     name: "chris.evans",
   },
+  {
+    profile:
+      "https://californiamuseum.org/wp-content/uploads/robertdowneyjr_cahalloffameinductee-1.png",
+    name: "_rober.d.jr",
+  },
+  {
+    profile:
+      "https://static.independent.co.uk/2022/08/22/10/mark%20zuckerberg%20metaverse%20avatar.png",
+    name: "m_zuck",
+  },
+
+  {
+    profile:
+      "https://img.championat.com/s/732x488/news/big/s/b/nikogda-ne-pejte-alkogol-pavel-durov-dal-sovet-molodym-lyudyam_17296969781139197240.jpg",
+    name: "durov.p.02",
+  },
+  {
+    profile:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdrqI27NF4kc--V4agJgGSFNZeb0GvZp5BFA&s",
+    name: "joe.russo",
+  },
+  {
+    profile:
+      "https://i.pinimg.com/564x/d3/10/78/d31078d949e74c2aa4244d44bd0f6574.jpg",
+    name: "kurosaki_17",
+  },
+  {
+    profile:
+      "https://wallpapers-clan.com/wp-content/uploads/2024/05/aesthetic-john-wick-yellow-desktop-wallpaper-preview.jpg",
+    name: "_john_w",
+  },
+
+  {
+    profile:
+      "https://ew.com/thmb/O5LusHiubmFfxX6DBjl4t56Bbgw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Chris-Evans-060124-800c2c0237574b2c81c140393ba20555.jpg",
+    name: "chris.evans",
+  },
 ];
